e996f3d27c69ecf91d2e3afe255c8cfbeb30cba2
Emily <emregolao@gmail.com>
2022-02-16 16:45:40 -0300

    creates Lead model

---
+++ app/models/leads_model.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from sqlalchemy import Column, String, Integer, DateTime
+from app.configs.database import db
+from dataclasses import dataclass
+
+@dataclass
+class Lead(db.Model):
+
+    id: int
+    name: str
+    email: str
+    phone: str
+    creation_date: datetime
+    last_visit: datetime
+    visits: int
+
+    __tablename__ = "leads"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    email = Column(String,unique=True, nullable=False)
+    phone = Column(String,unique=True, nullable=False)
+    creation_date = Column(DateTime,default=datetime.now())
+    last_visit = Column(DateTime)
+    visits = Column(Integer, default=1)
